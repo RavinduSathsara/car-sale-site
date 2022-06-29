75fbec8fa5d8e1090a10296fea2e0777bf290b94
LAPTOP-JBIFAPJM\ASUS <tsanjana350@gmail.com>
2022-06-29 23:31:02 +0530

    create testrun form

--- src/components/Forms/TestRun.jsx
+++ src/components/Forms/TestRun.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
 import moment from "moment";
-// import form "dateTimePicker1";
 
 const TestRun = () => {
   const [name, setName] = useState("");
