613363b8c65ca98c99965b5030335a35c5622229
ravindu.sathsara <ravindu.sathsara@inovaitsys.com>
2022-07-04 08:54:28 +0530

    add site vist

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -1,4 +1,15 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import moment from "moment";
+
 export const Features = (props) => {
+  useEffect(() => {
+    axios.post("http://127.0.0.1:8000/api/currentvisits", {
+      current_visits: 1,
+      month: moment(Date.now()).format("M"),
+    });
+  }, []);
+
   return (
     <div id="features" className="text-center text-block">
       <div className="container">
@@ -9,7 +20,6 @@ export const Features = (props) => {
           {props.data
             ? props.data.map((d, i) => (
                 <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
-                  {" "}
                   <i className={d.icon}></i>
                   <h3>{d.title}</h3>
                   <p>{d.text}</p>
