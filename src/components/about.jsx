593614e03e63a2598f26c2fcb4cf11b62e0e1a30
oshininawarathna <oshininawarathna9@gmail.com>
2022-07-08 14:04:30 +0530

    created about page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,41 +1,68 @@
 export const About = (props) => {
   return (
     <div id="about">
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-12 col-md-6">
-            {" "}
-            <img src="img/cars.jpg" className="img-responsive" alt="" />{" "}
-          </div>
-          <div className="col-xs-12 col-md-6">
-            <div className="about-text">
-              <h2>About Us</h2>
-              {/* <p>{props.data ? props.data.paragraph : "loading..."}</p> */}
-              {/* <h3>Why Choose Us?</h3> */}
-              <div className="list-style">
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  {/* <ul>
-                    {props.data
-                      ? props.data.Why.map((d, i) => (
-                          <li key={`${d}-${i}`}>{d}</li>
-                        ))
-                      : "loading"}
-                  </ul> */}
-                </div>
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  {/* <ul>
-                    {props.data
-                      ? props.data.Why2.map((d, i) => (
-                          <li key={`${d}-${i}`}> {d}</li>
-                        ))
-                      : "loading"}
-                  </ul> */}
-                </div>
+      <section className="about-section">
+        {" "}
+        <div className="container custom-container">
+          <div class="row">
+            <div class="col-lg-8 about-overview">
+              <div class="overview-inner">
+                <h1 class="overview-title">Our Beginnings</h1>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Deleniti a, cumque alias fuga, error consequuntur officiis
+                  nesciunt perspiciatis omnis ullam libero eaque suscipit. Fuga
+                  atque tempora adipisci nobis qui hic.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptatum praesentium, debitis molestias odio obcaecati non
+                  quod quasi beatae nisi nostrum ea! Ratione molestias
+                  voluptatem ducimus blanditiis. Maiores veniam recusandae
+                  excepturi!
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+                  necessitatibus nostrum veritatis aliquam excepturi est error
+                  temporibus quis, eos veniam et aperiam quaerat, tempora a
+                  mollitia sequi reiciendis nesciunt? Est?
+                </p>
+              </div>
+            </div>
+            <div class="col-lg-4 about-mission">
+              <div class="mission-inner">
+                <h3>Our Mission</h3>
+
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Error animi iusto possimus? Dignissimos culpa ea obcaecati
+                  asperiores aut delectus nemo deserunt, a corrupti vero
+                  sapiente mollitia ipsam quasi dicta. Harum?
+                </p>
+              </div>
+            </div>
+            <div class="col-lg-6 offset-lg-1 about-vission">
+              <div class="vission-inner">
+                <h3>Our Vission</h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+                  nihil error minima ad maxime inventore accusantium, non dolor
+                  velit facere labore? Nesciunt amet atque tenetur voluptate
+                  fugiat unde facere expedita.
+                </p>
+              </div>
+            </div>
+            <div class="col-lg-5 about-img">
+              <div class="img">
+                <img
+                  src="	https://www.unitedmotors.lk/images/xour-mission.jpg.pagespeed.ic.Dd1xWqk4yM.webp"
+                  alt=""
+                />
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 };
