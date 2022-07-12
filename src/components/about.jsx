7ba22bb044f104b144bf47185cf8d99d4205b3c6
dewyasara <dewniubesekara@gmail.com>
2022-07-12 14:21:29 +0530

    testimonials errors & about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -7,48 +7,45 @@ export const About = (props) => {
           <div class="row">
             <div class="col-lg-8 about-overview">
               <div class="overview-inner">
-                <h1 class="overview-title">Our Beginnings</h1>
+                <h1 class="overview-title">Our Mission</h1>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Deleniti a, cumque alias fuga, error consequuntur officiis
-                  nesciunt perspiciatis omnis ullam libero eaque suscipit. Fuga
-                  atque tempora adipisci nobis qui hic.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Voluptatum praesentium, debitis molestias odio obcaecati non
-                  quod quasi beatae nisi nostrum ea! Ratione molestias
-                  voluptatem ducimus blanditiis. Maiores veniam recusandae
-                  excepturi!
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-                  necessitatibus nostrum veritatis aliquam excepturi est error
-                  temporibus quis, eos veniam et aperiam quaerat, tempora a
-                  mollitia sequi reiciendis nesciunt? Est?
+                  Changing Perceptions About Car Buying. As people who genuinely
+                  care about our customers, we get that car buying can be
+                  intimidating. That's why we've committed to make everything
+                  simple. Our transparent pricing means the price you see is the
+                  price you'll pay. And we'll be here to help, making sure you
+                  drive away feeling completely satisfied.
                 </p>
               </div>
             </div>
             <div class="col-lg-4 about-mission">
               <div class="mission-inner">
-                <h3>Our Mission</h3>
+                <h3>Our Vision</h3>
 
                 <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Error animi iusto possimus? Dignissimos culpa ea obcaecati
-                  asperiores aut delectus nemo deserunt, a corrupti vero
-                  sapiente mollitia ipsam quasi dicta. Harum?
+                  The qualities that make Enterprise a trusted name in rental
+                  cars are the same qualities that make our car buying
+                  experience stand out-exceptional customer service and quality
+                  vehicle selection.
                 </p>
               </div>
             </div>
             <div class="col-lg-6 offset-lg-1 about-vission">
               <div class="vission-inner">
-                <h3>Our Vission</h3>
+                <h3>Our Values</h3>
+
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
-                  nihil error minima ad maxime inventore accusantium, non dolor
-                  velit facere labore? Nesciunt amet atque tenetur voluptate
-                  fugiat unde facere expedita.
+                  We believe that all decisions are to be made with integrity.
+                  Leadership is setting good examples for others to follow,
+                  expecting as much from yourself as from others.
+                </p>
+
+                <p>
+                  We believe we are responsible to satisfy the needs of our
+                  customers for all our products and services. Individual
+                  accountability for our own performance and for the quality of
+                  our products will demonstrate to our customers we are
+                  enthusiastic about them and really care.
                 </p>
               </div>
             </div>
