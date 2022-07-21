ce8ab68b1f408aecac3ab798bbad8aad126300ec
ravindu.sathsara <ravindu.sathsara@inovaitsys.com>
2022-07-21 21:06:42 +0530

    fixed

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react";
+import { Outlet, Link } from "react-router-dom";
+
 export const Services = (props) => {
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+  };
   return (
     <div id="services" className="text-center text-block">
       <div className="container text-upper">
@@ -10,20 +22,28 @@ export const Services = (props) => {
           {props.data
             ? props.data.map((d, i) => (
                 <div key={`${d.name}-${i}`} className="col-md-4">
-                  {" "}
-                  <i className={d.icon}></i>
+                  <Link target="_blank" to={d.link}>
+                    <i
+                      style={{
+                        color: isHovering ? "darkblue" : "",
+                      }}
+                      className={d.icon}
+                      onMouseEnter={handleMouseEnter}
+                      onMouseLeave={handleMouseLeave}
+                    ></i>
+                  </Link>
                   <div className="service-desc">
                     <h3>{d.name}</h3>
                     <p>{d.text}</p>
-                    {/* <a href="" target="_blank" to="/swap-deal">
-                      Swap Deal
-                      {d.link}
-                    </a> */}
+                    {/* <Link href="" target="_blank" to={d.link}>
+                      {d.name}
+                    </Link> */}
                   </div>
                 </div>
               ))
             : "loading"}
         </div>
+        <Outlet />
       </div>
     </div>
   );
