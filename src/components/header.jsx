cfcf077eb443b4c75208935ecb468fd7bb68f2ed
ravindu.sathsara <ravindu.sathsara@inovaitsys.com>
2022-07-22 22:12:35 +0530

    add carosol

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,32 +1,73 @@
 export const Header = (props) => {
   return (
-    <header id="header">
-      <div className="intro">
-        <div className="overlay">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-8 col-md-offset-2 intro-text">
-                <h1>
-                  {/* {props.data ? props.data.title : 'Loading'}
-                  <span></span> */}
-                  GET YOUR DREAM VEHICLE
-                </h1>
-                {/* <p>{props.data ? props.data.paragraph : "Loading"}</p> */}
-                <p>
-                  Choose your dream vehicle according to your own requirements
-                  from us.
-                </p>
-                <a
-                  href="#features"
-                  className="btn btn-custom btn-lg page-scroll"
-                >
-                  Learn More
-                </a>{" "}
-              </div>
+    <div>
+      <h2>Carousel Example</h2>
+      <div id="myCarousel" className="carousel slide" data-ride="carousel">
+        <ol className="carousel-indicators">
+          <li
+            data-target="#myCarousel"
+            data-slide-to="0"
+            className="active"
+          ></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <div className="carousel-inner">
+          <div className="item active">
+            <div className="carousel-caption">
+              <h3>Chicago</h3>
+              <p>Thank you, Chicago!</p>
             </div>
+            <img
+              src="https://images.unsplash.com/photo-1657299156537-f4bcdced5392?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              alt="Los Angeles"
+              style={{ width: "100%", height: "80%" }}
+            />
+          </div>
+
+          <div className="item">
+            <div className="carousel-caption">
+              <h3>Chicago</h3>
+              <p>Thank you, Chicago!</p>
+            </div>
+            <img
+              src="https://images.unsplash.com/photo-1553699357-b454793abefa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              alt="Chicago"
+              style={{ width: "100%", height: "80%" }}
+            />
+          </div>
+
+          <div className="item">
+            <div className="carousel-caption">
+              <h3>Chicago</h3>
+              <p>Thank you, Chicago!</p>
+            </div>
+            <img
+              src="https://images.unsplash.com/photo-1658351984016-dda2025086de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              alt="New york"
+              style={{ width: "100%", height: "80%" }}
+            />
           </div>
         </div>
+
+        <a
+          className="left carousel-control"
+          href="#myCarousel"
+          data-slide="prev"
+        >
+          <span className="glyphicon glyphicon-chevron-left"></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a
+          className="right carousel-control"
+          href="#myCarousel"
+          data-slide="next"
+        >
+          <span className="glyphicon glyphicon-chevron-right"></span>
+          <span className="sr-only">Next</span>
+        </a>
       </div>
-    </header>
+    </div>
   );
 };
